f1862940213c6e0371f98cb059080084db0cb36f
omerasipi <omer.asipi@stud.kftg.ch>
2018-05-15 21:05:33 +0200

    Shom i keq jom!

--- js/script.js
+++ js/script.js
@@ -15,3 +15,28 @@ $(function() {
 
     });
 });
+
+function JahrToBox() {
+    document.getElementById("JahrBox").value = document.getElementById("regler").value;
+
+}
+
+function BoxToJahr() {
+    document.getElementById("regler").value = document.getElementById("JahrBox").value;
+}
+
+function wahlenGen() {
+    var selektor = document.getElementById("wahlen"),
+        //Array muss mit csv reader ergänzt werden
+        arr = ["1","2","3"];
+
+    //Cleaner um alle Optionen zu entfernen
+    selektor.options.length = 0;
+    //Generator damit Optionen da sind
+    for (var i = 0; i < arr.length; i++) {
+        var option = document.createElement("OPTION"),
+            txt = document.createTextNode(arr[i]);
+        option.appendChild(txt);
+        selektor.insertBefore(option, selektor.lastChild);
+    }
+}
